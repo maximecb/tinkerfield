e3449854a92a80f5aabe57c9e314bb31a149ad39
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2026-03-15 09:54:40 -0400

    Add two math functions and an asphalt texture

--- src/math.rs
+++ src/math.rs
@@ -45,6 +45,14 @@ impl Vec3
             self
         }
     }
+
+    pub fn min(self, other: Self) -> Self {
+        Self::new(self.x.min(other.x), self.y.min(other.y), self.z.min(other.z))
+    }
+
+    pub fn max(self, other: Self) -> Self {
+        Self::new(self.x.max(other.x), self.y.max(other.y), self.z.max(other.z))
+    }
 }
 
 impl Add for Vec3 {
